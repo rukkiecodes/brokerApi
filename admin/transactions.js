ba5b567d87fcc04c099d0543d53fecdaaeb5ca0a
Terry Amagboro <rukkiecodes@gmail.com>
2022-09-16 00:35:36 +0100

    get transactions update

--- admin/transactions.js
+++ admin/transactions.js
@@ -1,10 +1,11 @@
 const router = require('express').Router()
 const Deposit = require('../models/deposit')
 const Transaction = require('../models/transaction')
+const Withdraw = require('../models/withraw')
 const checkAuth = require("../middleware/auth")
 
 router.post('/get', checkAuth, async (req, res) => {
-  let transactions = await Deposit.find()
+  let transactions = await Transaction.find()
 
   if (transactions.length >= 1)
     return res.status(200).json({
@@ -21,7 +22,7 @@ router.post('/get', checkAuth, async (req, res) => {
 router.post('/getTransactions', checkAuth, async (req, res) => {
   const { user } = req.body
 
-  let transactions = await Deposit.find({ user })
+  let transactions = await Transaction.find({ user })
 
   if (transactions.length >= 1)
     return res.status(200).json({
@@ -38,7 +39,7 @@ router.post('/getTransactions', checkAuth, async (req, res) => {
 router.post('/getSingleTransaction', checkAuth, async (req, res) => {
   const { _id } = req.body
 
-  let transaction = await Deposit.findOne({ _id })
+  let transaction = await Transaction.findOne({ _id })
 
   return res.status(200).json({
     message: "transaction found",
@@ -47,7 +48,7 @@ router.post('/getSingleTransaction', checkAuth, async (req, res) => {
 })
 
 router.get('/getPendingTransactions', checkAuth, async (req, res) => {
-  let transaction = await Deposit.find({ status: 'PENDING' })
+  let transaction = await Transaction.find({ status: 'PENDING' })
 
   return res.status(200).json({
     message: "transaction found",
@@ -56,7 +57,7 @@ router.get('/getPendingTransactions', checkAuth, async (req, res) => {
 })
 
 router.get('/getConfirmedTransactions', checkAuth, async (req, res) => {
-  let transaction = await Deposit.find({ status: 'CONFIRMED' })
+  let transaction = await Transaction.find({ status: 'CONFIRMED' })
 
   return res.status(200).json({
     message: "transaction found",
@@ -68,6 +69,8 @@ router.post('/confirmTransactionRequest', checkAuth, async (req, res) => {
   const { _id } = req.body
   try {
     const transaction = await Transaction.updateOne({ _id }, { $set: { status: 'CONFIRMED' } })
+    if (transaction.type == 'deposit') await Deposit.updateOne({ _id }, { $set: { status: 'CONFIRMED' } })
+    if (transaction.type == 'withdraw') await Withdraw.updateOne({ _id }, { $set: { status: 'CONFIRMED' } })
 
     res.json({
       transaction
