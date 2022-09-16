914f13b909f788789af860a8f124b2301961b0bd
Terry Amagboro <rukkiecodes@gmail.com>
2022-09-16 22:26:39 +0100

    made update

--- admin/auth/signup.js
+++ admin/auth/signup.js
@@ -16,7 +16,7 @@ const oAuth2Client = new google.auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECR
 oAuth2Client.setCredentials({ refresh_token: GOOGLE_REFRESH_TOKEN })
 
 router.post("/signup", async (req, res) => {
-  const { name, referal, email, phone, password } = req.body
+  const { name, email, password } = req.body
 
   try {
     let user = await Admin.findOne({ email })
--- models/admin.js
+++ models/admin.js
@@ -6,7 +6,6 @@ const adminSchema = new mongoose.Schema({
   avatar: { type: String },
   name: { type: String, required: true },
   email: { type: String, required: true },
-  phone: { type: String, required: true },
   password: { type: String, required: true },
   admin: Boolean,
   verified: Boolean
