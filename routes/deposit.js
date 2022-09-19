f3bafd122b2c3f27366b48e74453532833076988
Terry Amagboro <42579208+rukkiecodes@users.noreply.github.com>
2022-09-19 05:51:19 +0100

    Update deposit.js

--- routes/deposit.js
+++ routes/deposit.js
@@ -49,9 +49,9 @@ router.post('/add', upload.single('pop'), checkAuth, async (req, res) => {
     })
 
     if (user.investment) {
-      await User.updateOne({ user: user.id }, { $set: { investment: user.investment + amount } })
+      await User.updateOne({ _id: user._id }, { $set: { investment: user.investment + amount } })
     } else {
-      await User.updateOne({ user: user.id }, { $set: { investment: amount } })
+      await User.updateOne({ _id: user._id }, { $set: { investment: amount } })
     }
 
     return res.json({
