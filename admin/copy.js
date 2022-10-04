fbef01be02bdb3ad37c430488a2a5559341c0eb1
Terry Amagboro <42579208+rukkiecodes@users.noreply.github.com>
2022-10-04 17:08:18 +0100

    Update copy.js

--- admin/copy.js
+++ admin/copy.js
@@ -70,7 +70,7 @@ router.post('/editCopy', async (req, res) => {
   } else {
     try {
       let copy = await Copy.updateOne({ _id }, {
-        $set: { earnings: copy.profit * earnings, name, wins, losses, rate, profit, from, to, bankState, salesState, currency, amount }
+        $set: { name, wins, losses, rate, profit, from, to, bankState, salesState, currency, amount }
       })
       return res.status(200).json({
         message: "Copy updated",
