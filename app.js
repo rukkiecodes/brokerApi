d2d1addf3a07ac8a62b435b7f5a7ba4375549a1e
Terry Amagboro <42579208+rukkiecodes@users.noreply.github.com>
2023-03-21 20:02:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -98,7 +98,7 @@ app.use((req, res, next) => {
 })
 
 app.use((error, req, res, next) => {
-  res.status(error.status || 500)
+  await res.status(error.status || 500)
   res.json({
     error: {
       message: error.message,
